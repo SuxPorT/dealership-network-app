80ae653984642274258182347db42227f2c772d4
Alexys Santiago <SuxPorT@hotmail.com>
2023-10-31 21:25:55 -0300

    feat(api): Add packages and DbContext setup

---
+++ backend/DealershipNetworkApp.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DealershipNetworkApp.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
